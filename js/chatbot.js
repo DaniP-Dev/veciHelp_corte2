ca9dd991d65caa9667f96b365a6aab8cef50a742
Daniel Perez <dperezg23@outlook.com>
2025-02-19 15:47:33 -0500

    suiendo algo

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,16 +1,50 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const botonChatbot = () => {
     const chatbotComponente = document.getElementById('chatbotComponente');
     if (chatbotComponente.style.display === 'none' || chatbotComponente.style.display === '') {
         const iframe = document.createElement('iframe');
         iframe.src = '../html/chatbot.html';
-        iframe.style.width = '50%';
-        iframe.style.height = '500px';
+        iframe.style.width = '60%';
+        iframe.style.height = '600px';
         iframe.frameBorder = '0'; // Eliminar el borde del iframe
         iframe.style.position = 'fixed'; // Posición fija
         iframe.style.bottom = '60px'; // Ajustar la posición desde abajo a 60px
         iframe.style.right = '80px'; // Ajustar la posición desde la derecha
         iframe.style.transition = 'transform 0.3s ease-in-out'; // Añadir transición
         iframe.style.transform = 'translateY(100%)'; // Inicialmente fuera de la vista
+
+        // Media query para dispositivos móviles
+        if (window.matchMedia("(max-width: 768px)").matches) {
+            iframe.style.width = '70%';
+            iframe.style.height = '600px';
+            iframe.style.bottom = '20px';
+            iframe.style.right = '5%';
+            iframe.style.position = 'fixed'; // Posición fija
+            iframe.style.bottom = '60px'; // Ajustar la posición desde abajo a 60px
+            iframe.style.right = '80px'; // Ajustar la posición desde la derecha
+        }
 
         chatbotComponente.innerHTML = ''; // Limpiar el contenido del div
         chatbotComponente.appendChild(iframe); // Agregar el iframe al div
